6fa67569fd58a523a6e3bdaf632944fe28a1d229
mbaljeetsingh766 <bsinghs.007@gmail.com>
2020-07-07 10:58:23 +0530

    add scully config

--- scully.app.config.ts
+++ scully.app.config.ts
@@ -3,5 +3,13 @@ export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'app',
   outDir: './dist/static',
-  routes: {},
+  routes: {
+    '/todos/:id': {
+      type: 'json',
+      id: {
+        url: 'https://jsonplaceholder.typicode.com/todos',
+        property: 'id',
+      },
+    },
+  },
 };
